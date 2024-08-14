afe62e6c2335425b0761d06f8ff82141cd73d746
iZackk26 <newxstorecr@gmail.com>
2024-08-13 22:40:24 -0600

    feat: Adding print of how to access information in the smallest item of the stadium

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -5,5 +5,8 @@ mod stadium;
 
 fn main() {
     let mut stadium : HashMap<String, Zone> = stadium::data::generate_stadium();
-    println!("{:?}", stadium.get("Norte").unwrap().categories);
+    // println!("{:?}", stadium.get("Norte").unwrap().categories.get(&'A').unwrap().rows.len());
+    // println!("{:?}", stadium.get("Norte").unwrap().categories.get(&'A').unwrap().rows.get(&'W').unwrap().seats.len());
+    println!("{:?}", stadium.get("Sur").unwrap().categories.get(&'C').unwrap().rows.get(&'Y').unwrap().seats.get(&5));
+
 }
