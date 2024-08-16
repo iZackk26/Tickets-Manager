2a2936f7e934391bf6affe7eb5134c4c4e059dcc
akhoz <adrian25092005@gmail.com>
2024-08-16 14:59:08 -0600

    merge

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -2,8 +2,10 @@ use std::collections::HashMap;
 use crate::stadium::structures::Zone;
 
 mod stadium;
+mod algorithm;
 
 fn main() {
-    let mut stadium : HashMap<String, Zone> = stadium::data::generate_stadium();
-    println!("{:?}", stadium.get("North").unwrap().categories.get(&'A').unwrap().rows.get(&'W').unwrap().seats);
+    //let mut stadium : HashMap<String, Zone> = stadium::data::generate_stadium();
+    //println!("{:?}", stadium);
+    algorithm::test();
 }
