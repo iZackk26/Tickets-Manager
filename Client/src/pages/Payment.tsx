b50d84ea7f968998f2f124856af11f51a424c139
iZackk__26 <newxstorecr@gmail.com>
2024-10-28 21:48:53 -0600

    fix: finished version of the payment form, need to make a redirect to home

--- Client/src/pages/Payment.tsx
+++ Client/src/pages/Payment.tsx
@@ -1,10 +1,10 @@
-import DinamicPluginLoader from "../DinamicPluginLoader"
+import DinamicPluginLoader from "../DinamicPluginLoader";
 import SeatType from "../types/Seat";
 import BaseComponent from "../classes/BaseComponent";
 import AxiosService from "../classes/AxiosService";
 import ROUTES from "../constants/routes";
 
-interface Props { }
+interface Props {}
 
 interface State {
   seats: SeatType[];
@@ -19,23 +19,33 @@ class Payment extends BaseComponent<Props, State> {
     };
   }
 
-  handlePayClick = () => {
-    const data {
+  handlePayClick = async () => {
+    try {
+      // Los datos que quieres enviar al backend
+      const data = this.state.seats;
 
+      // Realizar la solicitud POST al backend
+      const response = await AxiosService.getInstance().post(
+        ROUTES.modifySeats,
+        data
+      );
+
+      // Puedes manejar la respuesta según lo que devuelva el backend
+      console.log("Respuesta del backend:", response.data);
+    } catch (error) {
+      console.error("Error al modificar los asientos:", error);
     }
-
-
-    const response = await AxiosService.getInstance().post(
-      ROUTES.modifySeats,
-  }
+  };
 
   render() {
     return (
       <>
         <div className="flex flex-col items-center justify-center space-y-5 w-1/2">
           <DinamicPluginLoader />
-          <button className="bg-black text-white py-3 px-10 rounded-lg mt-4"
-            onClick={this.handlePayClick}>
+          <button
+            className="bg-black text-white py-3 px-10 rounded-lg mt-4"
+            onClick={this.handlePayClick}
+          >
             Pay
           </button>
         </div>
